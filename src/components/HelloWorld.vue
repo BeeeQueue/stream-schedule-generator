1c6d527b409225073e74545ef6badc32de181c07
Adam <adam.lj.haglund@gmail.com>
2020-12-13 21:41:08 +0100

    prettier write

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,41 +7,50 @@
     <a
       href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
       target="_blank"
-      >Vetur</a
     >
+      Vetur
+    </a>
     +
     <a
       href="https://marketplace.visualstudio.com/items?itemName=znck.vue-language-features-insiders"
       target="_blank"
-      >Vue Language Features (Insiders)</a
     >
+      Vue Language Features (Insiders)
+    </a>
   </p>
+
   <p>
     Make sure to use workspace version of TypeScript to get improved support via
-    <a href="https://github.com/znck/vue-developer-experience" target="_blank"
-      >@vuedx</a
-    >. Note @vuedx is still experimental and this setup is provided for early feedback.
+    <a href="https://github.com/znck/vue-developer-experience" target="_blank">
+      @vuedx
+    </a>
+    . Note @vuedx is still experimental and this setup is provided for early
+    feedback.
   </p>
+
   <button @click="count++">count is: {{ count }}</button>
+
   <p>
-    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+    Edit
+    <code>components/HelloWorld.vue</code>
+    to test hot module replacement.
   </p>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue"
 
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup: () => {
     const count = ref(0)
     return { count }
-  }
+  },
 })
 </script>
